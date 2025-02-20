84b9dfe71c41fddbdb720d50d5ddba32c5ac9cff
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2025-02-20 12:34:36 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var link="https://jsonblob.com/api/1342186332981747712"
+var arn=[];
+
+
 let currentAIndex = 0;
 window.onload = () => createNavBar();
 const handleActiveNavItem = () => {
@@ -12,6 +16,19 @@ const handleActiveNavItem = () => {
   }
 };
 const createNavBar = () => {
+
+  GetJ(link,function(r){
+    arn=r;
+   //if(r!=undefined){
+   //  alert(JSON.stringify(arn))
+   //}
+  if(arn.length>0){
+    allA=allA.concat(arn);
+  //  alert(allA)
+  }
+  
+
+
   const navbar = document.querySelector(".navbar");
   for (let i = 0; i < allA.length; i++) {
     const button = document.createElement("button");
@@ -22,10 +39,15 @@ const createNavBar = () => {
       handleActiveNavItem();
       createSelect();
       calculateUnit();
+      tequiv.value=JSON.stringify(allA[i]);
     };
     navbar.appendChild(button);
   }
   handleActiveNavItem();
+
+
+})
+
 };
 
 const fromSelect = document.getElementById("from-select");
@@ -83,7 +105,7 @@ const calculateUnit = () => {
   }
   const iv = parseFloat(value);
   const stdVal = eval(allA[currentAIndex][selectFromId][1]);
- // alert(allA[currentAIndex][selectFromId][1]+"\n"+stdVal+"\n"+iv+"\n"+value)
+  alert(allA[currentAIndex][selectFromId][1]+"\n"+stdVal+"\n"+iv+"\n"+value)
   for (let i = 1; i < allA[currentAIndex].length; i++) {
     const temp = calVal(i, stdVal);
     toSelect.options[i - 1].text =
@@ -97,3 +119,59 @@ const calculateUnit = () => {
     fromSelect.options[selectFromId - 1].text
   } = ${toInput.value} ${fromSelect.options[selectToId - 1].text}</p>`;
 };
+
+
+function addEq(){
+  if(tequiv.value!=""){
+    var a=JSON.parse(tequiv.value);
+if(Array.isArray(a)){
+ arn.push(a);
+ //alert(arn);
+ if(confirm("New equivalence will be added, ok to procede")){
+   UpdateJ(link,arn,function(r){alert("all done,app will be restarted...");window.location.reload()})
+ }
+}
+else alert("It is not an array")
+}
+}
+
+function GetJ(url, callback) {
+
+//var url  = "http://localhost:8080/api/v1/users";
+var xhr = new XMLHttpRequest()
+xhr.open('GET', url + '/1', true)
+xhr.onload = function() {
+    //         (xhr.responseText)
+    var users = JSON.parse(xhr.responseText);
+    //     var obb=decoding(users.prim);
+
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        callback(users);
+    } else {
+        console.error(users);
+    }
+}
+xhr.send(null);
+}
+
+function UpdateJ(url, ob, callback) {
+
+json = JSON.stringify(ob);
+var xhr = new XMLHttpRequest();
+xhr.open("PUT", url + '/12', true);
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+xhr.onload = function() {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        //  alert("Tu Forma ha sido Enviada");
+        if (callback != undefined) callback(users);
+    } else {
+        (users);
+    }
+}
+xhr.send(json);
+}
+
+
+
+
